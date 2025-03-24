15e1005953d3c64d856bcd275cd57ed55c21d8a0
Carlos Santos <carlos.santos@premium-minds.com>
2025-03-24 15:34:54 +0000

    added plate edit

--- pushdev.sh
+++ pushdev.sh
@@ -1,4 +1,4 @@
-docker-compose down && docker-compose up -d --build && docker-compose logs -f
+docker-compose down && docker-compose up -d --build 
 docker image rm karlos2/frontend-matriculas:dev
 docker image rm karlos2/backend-matriculas:dev
 docker image tag matricula-app_frontend karlos2/frontend-matriculas:dev
@@ -6,3 +6,4 @@ docker image tag matricula-app_backend karlos2/backend-matriculas:dev
 docker push karlos2/frontend-matriculas:dev
 docker push karlos2/backend-matriculas:dev
 docker image ls
+docker-compose logs -f
