7cd016a7868ed4ae6cd8def74c843cfc71dffeaf
Carlos Santos <carlos.santos@premium-minds.com>
2025-09-28 01:54:44 +0100

    .

--- pushprod.sh
+++ pushprod.sh
@@ -1,4 +1,4 @@
-docker-compose down && docker-compose up -d --build
+docker-compose build --no-cache frontend backend
 docker image rm karlos2/frontend-matriculas:latest
 docker image rm karlos2/backend-matriculas:latest
 docker image tag matricula-app_frontend karlos2/frontend-matriculas:latest
@@ -6,4 +6,3 @@ docker image tag matricula-app_backend karlos2/backend-matriculas:latest
 docker push karlos2/frontend-matriculas:latest
 docker push karlos2/backend-matriculas:latest
 docker image ls
-docker-compose logs -f
