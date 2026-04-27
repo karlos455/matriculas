09e99eb9180db6c46244dd199010b1e75e7d854f
Carlos Santos <carlos.santos@premium-minds.com>
2026-04-27 15:33:08 +0100

    change pushprod

--- pushprod.sh
+++ pushprod.sh
@@ -9,8 +9,8 @@ unset DOCKER_DEFAULT_PLATFORM
 
 docker image rm karlos2/frontend-matriculas:latest >/dev/null 2>&1 || true
 docker image rm karlos2/backend-matriculas:latest >/dev/null 2>&1 || true
-docker image tag matricula-app_frontend karlos2/frontend-matriculas:latest
-docker image tag matricula-app_backend karlos2/backend-matriculas:latest
+docker image tag matriculas-frontend karlos2/frontend-matriculas:latest
+docker image tag matriculas-backend karlos2/backend-matriculas:latest
 docker push karlos2/frontend-matriculas:latest
 docker push karlos2/backend-matriculas:latest
 docker image ls
